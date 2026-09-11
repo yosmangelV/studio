1f8a6694b5d96d3bc9e912f9368f5a9eaadf75b4
Yosmangel Adames <yosmangel.yk@gmail.com>
2026-09-11 22:52:20 +0200

    fix(announcement-web): update apiUrl to Render deployment

--- projects/announcement-web/src/environments/environment.prod.ts
+++ projects/announcement-web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://announcement-api-production.up.railway.app/api',
+  apiUrl: 'https://announcement-api-le0i.onrender.com/api',
 };
